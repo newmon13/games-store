a7f58798938574deb07130dcea80728429aa9a74
jakublipka <kubalipka123@gmail.com>
2026-05-09 12:04:54 +0200

    added accessors

--- src/main/java/dev/jlipka/recrutly/gamesstore/game/review/Review.java
+++ src/main/java/dev/jlipka/recrutly/gamesstore/game/review/Review.java
@@ -35,4 +35,28 @@ public class Review {
     public Long getId() {
         return id;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
